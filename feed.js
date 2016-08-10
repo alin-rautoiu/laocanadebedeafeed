bbdd364a261c330c501c5bdb7dc0297103dd245d
Alin <alin.ratuoiu@cs.pub.ro>
2016-08-10 23:28:55 +0300

    initial commit

---
+++ feed.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var builder = require('xmlbuilder');
+
+// var feed = {
+//     'rss' : {
+//         '@version' : '2.0'
+//     },
+//     'channel' : 
+//     {
+//         'title' : 'La o cană de bedea',
+//         'link' : 'https://archive.org/details/@alinrautoiu?and[]=subject%3A%22la+o+cana+de+bedea%22',
+//         'description' : 'Podcast unde Alin discuta despre benzi desenate cu artisti de benzi desenate',
+//         'item' : {
+//             'title' : 'Ep. 1',
+//             'link' : 'https://archive.org/download/interviu_prandea/interviu_prandea_vbr.m3u',
+//             'description' : 'Ep. 1 cu Cristian Prandea'
+//         },
+//         'item' : {
+//             'title' : 'Ep. 1',
+//             'link' : 'https://archive.org/download/interviu_prandea/interviu_prandea_vbr.m3u',
+//             'description' : 'Ep. 1 cu Cristian Prandea'
+//         },
+//         'item' : {
+//             'title' : 'Ep. 1',
+//             'link' : 'https://archive.org/download/interviu_prandea/interviu_prandea_vbr.m3u',
+//             'description' : 'Ep. 1 cu Cristian Prandea'
+//         },
+        
+//     }
+// };
+
+var root = builder
+    .create('rss')
+        .att('version', '2.0')
+        .ele('channel')
+            .ele('title', 'La o cană de bedea')
+                .up()
+            .ele('link', 'https://archive.org/details/@alinrautoiu?and[]=subject%3A%22la+o+cana+de+bedea%22')
+                .up()
+            .ele('description', 'Podcast unde Alin discuta despre benzi desenate cu artisti de benzi desenate')
+                .up()
+            .ele('item')
+                .ele('title', 'Episodul 1')
+                    .up()
+                .ele('link', 'https://archive.org/download/interviu_prandea/interviu_prandea_vbr.m3u')
+                    .up()
+                .ele('description', 'Ep. 1 cu Cristian Prandea')
+                    .up()
+                .up()
+            .ele('item')
+                .ele('title', 'Episodul 2 - Giant Days cu Anna Benczédi')
+                    .up()
+                .ele('link', 'https://archive.org/download/alins_interviu_anna_modificat_la_greu/alins_interviu_anna_modificat_la_greu.mp3')
+                    .up()
+                .ele('description', 'Alături de Anna Benczédi discutăm despre Giant Days. Anna este co-autoarea webcomicului Știma Apelor și autoarea povestirii Jurnalul de trei parale din Țara Desenată.')
+                    .up()
+                .up()
+            .ele('item')
+                .ele('title', 'Episodul 3 - One-Punch Man cu Elena Barbu')
+                    .up()
+                .ele('link', 'https://archive.org/download/elena_one_punch_man/elena_one_punch_man.mp3')
+                    .up()
+                .ele('description', 'Ep. 1 cu Cristian Prandea')
+                    .up()
+                .up();
+
+var app = express();
+
+app.get('/bedea.rss', function(req, res) {
+
+    var xml = root.end({ pretty: true}); 
+    res.setHeader('Content-Type', 'application/xml');
+    res.send(xml);
+    return;
+});
+var server = app.listen(process.env.PORT || 3002);
